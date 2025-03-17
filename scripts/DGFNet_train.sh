1e12feaa1280883f31aae69c19cf840fc18eabc2
GuiPengXin <53513246+XinGP@users.noreply.github.com>
2025-03-17 17:11:22 +0800

    Update DGFNet_train.sh

--- scripts/DGFNet_train.sh
+++ scripts/DGFNet_train.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python train.py \
   --features_dir data_argo/features/ \
-  --train_batch_size 1 \
+  --train_batch_size 16 \
   --val_batch_size 16 \
   --val_interval 2 \
   --train_epoches 50 \
